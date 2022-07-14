aaf44ea1e2d07a14b65889429f89a610d609c46a
Antgitgut <antcheery@gmail.com>
2022-07-14 09:13:12 +0300

    Fixed code and added annotation.

---
+++ src/test/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        /**
+         * value 1
+         * value 2
+         */ //the comment block is a reminder
+        int x = 1;
+        int y = x+ 1;
+        System.out.println(y);
+    }
+}
+//I wrote my first code in Java!
+
