fe1964ce695c0ac09fa0b65b43b45c25a0258ab4
Antgitgut <antcheery@gmail.com>
2022-07-19 17:52:39 +0300

    Some ways of creating a class.

---
+++ src/test/java/Dog.java
@@ -0,0 +1,65 @@
+public class Dog {
+    String breed;
+    String name;
+    int age;
+    int paws;
+
+
+    public static void main(String[] args) {
+        Dog Brownie = new Dog();
+        Brownie.breed = "pincher";
+        Brownie.name = "Brownie";
+        Brownie.age = 6;
+        Brownie.paws = 4;
+
+        System.out.println(Brownie.name + " " + Brownie.age);
+
+
+    }
+}
+
+/** Without parameters:
+ *
+ * public Dog(String name, int age){
+ *     this.name = name;
+ *     this.age = age;
+ *
+ * }
+ * public static void main(String[] args) {
+ *      Dog Brownie = new Dog(name: "Brownie", age: 5);
+ *
+ *      System.out.println(Brownie.name);
+ *      System.out.println(Brownie.age);
+ *
+ * OR:
+ *
+ *      Dog Shelly = new Dog(name: "Shelly", age: 7);
+ *      Dog Michael = new Dog(name: "Michael", age: 11);
+ *
+ *      System.out.println(Shelly.name + " " + Shelly.age);
+ *      System.out.println(Michael.name + " " + Michael.age);
+ *
+ * }
+*/
+
+/**
+ * The 3rd variant:
+ *
+ * public class Main {
+ *
+ *     public static void main(String[] args) {
+ *         Person Tom = new Person();
+ *         Tom.name = "Tom";
+ *         Brownie.age = 46;
+ *
+ *         System.out.println(Tom.name);
+ *         System.out.println(Tom.age);
+ *     }
+ *
+ *      static class Person{
+ *          String name;
+ *          int age:
+ *      }
+ * }
+ */
+
